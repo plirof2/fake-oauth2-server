beba294704d8226075e318a1f5d43fda5adc3973
aaa <aaa@aaa.com>
2018-05-29 11:40:59 +0300

    added formspde-test callback allowed

--- export_variable_settings_doatap.sh
+++ export_variable_settings_doatap.sh
@@ -9,7 +9,13 @@ export ACCESS_TOKEN_REQUEST_PATH=/oauth2/v4/token
 export USERINFO_REQUEST_URL=/oauth2/v3/userinfo
 echo ">>>|The HTTP path of the [access token request](https://tools.ietf.org/html/rfc6749#section-4.1.3) which the fake server listens on|"
 #export PERMITTED_REDIRECT_URLS=http://localhost/drupal8test/gsis,http://localhost:8181/auth/login,http://localhost/drupal84/gsis
-export PERMITTED_REDIRECT_URLS=http://localhost/drupal84/gsis,http://localhost/formspde-test/oauth2/authorized
+export PERMITTED_REDIRECT_URLS=http://localhost/drupal84/gsis,http://192.168.1.142/lara3/api/callback
 echo ">>>|comma-separated list of permitted [redirection endpoints]("
+
+
+echo "$this->tokenUrl = 'http://localhost:8282/oauth2/v4/token'";
+echo "$this->userinfoUrl = 'http://localhost:8282/oauth2/v3/userinfo'";
+echo "$this->authorizeUrl = 'http://localhost:8282/o/oauth2/v2/auth'";
+
 
 node server.js
